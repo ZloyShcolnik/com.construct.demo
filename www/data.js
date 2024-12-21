379d243086d709a96012856793c3d3f3f07085a4
ZloyShcolnik <zloalways7@gmail.com>
2024-12-21 03:31:10 +0300

    init

---
+++ www/data.js
@@ -0,0 +1,1 @@
+﻿{"project": [null,"Layout 1",[],[],[],[["Layout 1",480,854,false,"Event sheet 1",301895631590674,[["Layer 0",0,419906230568079,true,[255,255,255],false,1,1,1,false,false,1,0,0,[],[]]],[],[]]],[["Event sheet 1",[]]],[],"media/",false,480,854,4,true,true,true,"1.0.0.0",true,false,1,0,0,false,true,1,true,"com.construct.demo",0,[]]}
